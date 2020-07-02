d44de7889ea983032ad791c62fb1dded15b25bbe
alinan-vn <jandrolinan@gmail.com>
2020-07-02 13:47:47 -0500

    contact compononent and styling

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import Footer from '../global/footer/index';
 
 class Contact extends React.Component {
     render(){
         return (
-            <section>
-                <h1>This is the Contact page</h1>
+            <section className="contact">
+                <h2>Contact Me..</h2>
+
+                <div className="contact__list">
+                    <div className="contact__email">
+                        {/* <i className="fas fa-envelope"></i> Email */}
+                        <div className="text-secondary">jandrolinan@gmail.com</div>
+                    </div>
+                    <div className="contact__phone">
+                        {/* <i className="fas fa-mobile-alt"></i> Phone */}
+                        <div className="text-secondary">+1 (312) 888 - 1838</div>
+                    </div>
+                    <div className="contact__address">
+                        {/* <i className="fas fa-city"></i> Local At */}
+                        <div className="text-secondary">Chicago, IL</div>
+                    </div>
+                </div>
+
+                <Footer />
             </section>
         )
     }
